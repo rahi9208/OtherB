e9b6f451a622314db21b0bacaed8c30396e5d50c
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-14 12:02:31 +0530

    Added translation

--- OtherB/list.js
+++ OtherB/list.js
@@ -1,5 +1,6 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
+let translate = new AWS.Translate();
 exports.handler = function (event, context, callback) {
 	let response = {
 		"isBase64Encoded": 1,
@@ -19,11 +20,13 @@ exports.handler = function (event, context, callback) {
 			':it': itemType
 		},
 		FilterExpression: 'itemType = :it'
-	}, function (err, data) {
+	}, async function (err, data) {
 		if (!err && data.Items) {
-			response.body = JSON.stringify(data.Items.map((item)=>{
-				item.image = "https://s3.amazonaws.com/"+
-			}));
+			response.body = JSON.stringify(await Promise.all(data.Items.map(async (item) => {
+				item.image = "https://s3.amazonaws.com/" + process.env["IMAGE_BUCKET"] + "/" + item.itemCode + ".jpg";
+				item.itemName = await translateName(item.itemName, "zh");
+				return item;
+			})));
 		} else {
 			response.statusCode = 404;
 			response.body = "No Items";
@@ -34,4 +37,8 @@ exports.handler = function (event, context, callback) {
 
 
 
+}
+
+async function translateName(name, to) {
+	return (await translate.translateText({ SourceLanguageCode: "en", TargetLanguageCode: to, Text: name }).promise()).TranslatedText;
 }
